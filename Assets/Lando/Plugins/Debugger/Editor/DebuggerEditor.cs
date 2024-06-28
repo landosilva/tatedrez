7b4def4c7b689d4f965bd6b370c51543e114b8e1
Lando Silva <orlandos@zeptolab.com>
2024-06-29 00:07:38 +0200

    ✨ feat: added option on debugger to clear individual logs

--- Assets/Lando/Plugins/Debugger/Editor/DebuggerEditor.cs
+++ Assets/Lando/Plugins/Debugger/Editor/DebuggerEditor.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Lando.Core.Extensions;
 using UnityEditor;
 using UnityEngine;
@@ -19,18 +20,21 @@ namespace Lando.Plugins.Debugger.Editor
             const float nameWidth = 200;
             const float colorWidth = 70;
             const float enabledWidth = 50;
+            const float trashButtonWidth = 32;
             
             GUILayoutOption[] nameOptions = {GUILayout.Width(nameWidth), GUILayout.ExpandWidth(true)};
             GUILayoutOption[] colorOptions = {GUILayout.Width(colorWidth)};
             GUILayoutOption[] enabledOptions = {GUILayout.Width(enabledWidth)};
+            GUILayoutOption[] trashButtonOptions = {GUILayout.Width(trashButtonWidth)};
             
             EditorGUILayout.BeginHorizontal();
             GUILayout.Label(text: "Name", EditorStyles.boldLabel, nameOptions);
             GUILayout.Label(text: "Color", EditorStyles.boldLabel, colorOptions);
             GUILayout.Label(text: "Enabled", EditorStyles.boldLabel, enabledOptions);
+            GUILayout.Label(text: "", EditorStyles.boldLabel, trashButtonOptions);
             EditorGUILayout.EndHorizontal();
             
-            foreach (Debugger.DebugSource source in debugger.Sources)
+            foreach (Debugger.DebugSource source in debugger.Sources.ToList())
             {
                 EditorGUILayout.BeginHorizontal();
                 GUILayout.Label(source.Name, nameOptions);
@@ -39,6 +43,11 @@ namespace Lando.Plugins.Debugger.Editor
                 if (GUILayout.Button(source.Enabled ? "✓" : "╳", enabledOptions))
                     source.Enabled = !source.Enabled;
                 GUI.backgroundColor = Color.white;
+                
+                GUIContent trashIcon = EditorGUIUtility.IconContent(name: "TreeEditor.Trash");
+                if (GUILayout.Button(trashIcon, trashButtonOptions))
+                    debugger.Sources.Remove(source);
+                
                 EditorGUILayout.EndHorizontal();
             }
             
